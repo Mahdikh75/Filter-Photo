b0fd33f5f07a07eb94bae217f3328962318b8be6
Mahdi khayamdar <48985789+Mahdikh75@users.noreply.github.com>
2021-04-30 14:26:46 +0430

    Initial commit

---
+++ Filter.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Filter_Photo
+{
+    public class Filter
+    {
+        // ints : Color Filter
+        private int PxR, PxG, PxB;
+        private Image image;
+
+        public Filter(Image image)
+        {
+            this.image = image;
+        }
+
+        public Image GrayScale()
+        {
+            Bitmap bitmap = new Bitmap(image);
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color Avg_Color = bitmap.GetPixel(i, j);
+                    int PX_Color_New = (int)((Avg_Color.R + Avg_Color.B + Avg_Color.G) / 3);
+                    bitmap.SetPixel(i, j, Color.FromArgb(PX_Color_New, PX_Color_New, PX_Color_New));
+                }
+            }
+            return bitmap;
+        }
+
+        public Image Invert()
+        {
+            Bitmap bitmap = new Bitmap(image);
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color Old = bitmap.GetPixel(i, j);
+                    Color New = Color.FromArgb(255 - Old.R, 255 - Old.G, 255 - Old.B);
+                    bitmap.SetPixel(i, j, New);
+                }
+            }
+            return bitmap;
+        }
+
+        public enum TypeColor
+        {
+            red, green, blue
+        }
+
+        // ColorFilter [0 to 255] (value color)
+        public Image ColorFilter(TypeColor typeColor, int value_color)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            if (value_color >= 0 && value_color <= 255)
+            {
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color color = bitmap.GetPixel(i, j);
+                        switch (typeColor)
+                        {
+                            case TypeColor.red:
+                                PxR = color.R;
+                                PxG = color.G - value_color;
+                                PxB = color.B - value_color;
+                                break;
+                            case TypeColor.green:
+                                PxR = color.R - value_color;
+                                PxG = color.G;
+                                PxB = color.B - value_color;
+                                break;
+                            case TypeColor.blue:
+                                PxR = color.R - value_color;
+                                PxG = color.G - value_color;
+                                PxB = color.B;
+                                break;
+                            default:
+                                break;
+                        }
+                        PxR = Math.Max(PxR, 0);
+                        PxR = Math.Min(PxR, 255);
+
+                        PxG = Math.Max(PxG, 0);
+                        PxG = Math.Min(PxG, 255);
+
+                        PxB = Math.Max(PxB, 0);
+                        PxB = Math.Min(PxB, 255);
+
+                        bitmap.SetPixel(i, j, Color.FromArgb(PxR, PxG, PxB));
+                    }
+                }
+            }
+            return bitmap;
+        }
+
+        // Gamma [0.2 to 5] (colors)
+        private byte[] GM_Array(double dfcolor)
+        {
+            byte[] gm = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                gm[i] = (byte)Math.Min(255, (int)(255.0 * Math.Pow(i / 255.0, 1.0 / dfcolor) + 0.5));
+            }
+            return gm;
+        }
+
+        public Image Gamma(double red, double green, double blue)
+        {
+            Bitmap bitmap = new Bitmap(image);
+
+            byte[] red_gm = GM_Array(red);
+            byte[] green_gm = GM_Array(green);
+            byte[] blue_gm = GM_Array(blue);
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color old = bitmap.GetPixel(i, j);
+                    bitmap.SetPixel(i, j, Color.FromArgb(red_gm[old.R], green_gm[old.G], blue_gm[old.B]));
+                }
+            }
+            return bitmap;
+        }
+
+        // Brightness [-255 to +255] (value)
+        public Image Brightness(int value)
+        {
+            Bitmap bitmap = new Bitmap(image);
+
+            if (value > 255)
+                value = 255;
+
+            if (value < -255)
+                value = -255;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    int R = color.R + value;
+                    int G = color.G + value;
+                    int B = color.B + value;
+
+                    R = R > 255 ? 255 : R;
+                    R = R < 0 ? 1 : R;
+
+                    G = G > 255 ? 255 : G;
+                    G = G < 0 ? 1 : G;
+
+                    B = B > 255 ? 255 : B;
+                    B = B < 0 ? 1 : B;
+
+                    bitmap.SetPixel(i, j, Color.FromArgb(R, G, B));
+                }
+            }
+            return bitmap;
+        }
+
+        // Contrast [-100 to 100] (Value)
+        public Image Contrast(double value)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            if (value > 100)
+                value = 100;
+            if (value < -100)
+                value = -100;
+
+            value = (100.0 + value) / 100.0;
+            value *= value;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+
+                    double R = color.R / 255.0;
+                    R -= 0.5;
+                    R *= value;
+                    R += 0.5;
+                    R *= 255;
+                    R = R > 255 ? 255 : R;
+                    R = R < 0 ? 0 : R;
+
+                    double G = color.G / 255.0;
+                    G -= 0.5;
+                    G *= value;
+                    G += 0.5;
+                    G *= 255;
+                    G = G > 255 ? 255 : G;
+                    G = G < 0 ? 0 : G;
+
+                    double B = color.B / 255.0;
+                    B -= 0.5;
+                    B *= value;
+                    B += 0.5;
+                    B *= 255;
+                    B = B > 255 ? 255 : B;
+                    B = B < 0 ? 0 : B;
+
+                    bitmap.SetPixel(i, j, Color.FromArgb((int)R, (int)G, (int)B));
+                }
+            }
+
+            return bitmap;
+        }
+
+    }
+}
---
+++ Photo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Filter_Photo
+{
+    public class Photo
+    {
+        // Basic changes ==> resize - screenshot - crop image - write text on images
+        public Image Resize(Image image, Size size)
+        {
+            return new Bitmap(image, size);
+        }
+
+        public Image Crop(Image image, int x, int y, int W, int H)
+        {
+            return new Bitmap(image).Clone(new Rectangle(x, y, W, H), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        }
+
+        public Image PS_Image(string text, Font font, Color color, Image image, Point point)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            SolidBrush solidBrush = new SolidBrush(color);
+            graphics.DrawString(text, font, solidBrush, point);
+            return bitmap;
+        }
+
+        public Image ScreenShot(int W, int H)
+        {
+            Bitmap bitmap = new Bitmap(W, H);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(Point.Empty, Point.Empty, new Size(W, H));
+            return bitmap;
+        }
+
+    }
+}
